1d83e37d7c8d451ea91a54368c529606d678d542
Son Luong Ngoc <sluongng@gmail.com>
2022-05-15 12:43:48 +0200

    staticcheck: rename test func
    It should not be HelloWorld, but TestAnalyzer

--- staticcheck/analyzer_test.go
+++ staticcheck/analyzer_test.go
@@ -2,7 +2,7 @@ package analyzer
 
 import "testing"
 
-func TestHelloWorld(t *testing.T) {
+func TestAnalyzer(t *testing.T) {
 	if name != Analyzer.Name {
 		t.Errorf("want %s, got %s", name, Analyzer.Name)
 	}
