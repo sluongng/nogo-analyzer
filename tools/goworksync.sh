c265e3369b30d8891a1d992f98b33db70bb78f97
Son Luong Ngoc <sluongng@gmail.com>
2022-05-28 08:44:44 +0200

    goworksync: include running go mod tidy
    Find all the 'go.mod' files in the repository, then change to it's
    directory and run 'go mod tidy'.  This helps us ensure that all deps
    information are within the go.mod files and thus help 'go work sync'
    work properly.

--- tools/goworksync.sh
+++ tools/goworksync.sh
@@ -3,6 +3,17 @@
 GOBIN=$1
 
 (
-    cd $BUILD_WORKSPACE_DIRECTORY
-    $(bazel info execution_root)/$GOBIN work sync
+	cd "$BUILD_WORKSPACE_DIRECTORY" || exit
+	GO="$(bazel info execution_root)/$GOBIN"
+
+	find . -name go.mod |
+		while IFS= read -r -d '' f; do
+			d=$(dirname "$f")
+			(
+				cd "$d" || exit
+				"$GO" mod tidy
+			)
+		done
+
+	"$GO" work sync
 )
